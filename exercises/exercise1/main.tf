8c3080088837c6504f46eabb01078a33b2234cd6
Warner Bell <warner.bell21@gmail.com>
2023-11-13 10:31:44 -0600

    update exercise

--- exercises/exercise1/main.tf
+++ exercises/exercise1/main.tf
@@ -1,9 +1,8 @@
 terraform {
-  required_version = ">= 1.4.0"
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.0.0"
+      source = "hashicorp/aws"
+      version = "5.25.0"
     }
   }
 }
